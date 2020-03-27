64e10b03b7560fea6f4e18b627b108d894e92b26
박관우 <pgw9470@naver.com>
2020-03-27 13:14:56 +0900

    ㅇ

--- jblog06/src/main/java/com/douzone/jblog/BootApp.java
+++ jblog06/src/main/java/com/douzone/jblog/BootApp.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BootApp {
 	public static void main(String[] args) {
-		SpringApplication.run(BootApp.class, args);
+		SpringApplication.run(BootApp.class, args );
 	}
 }
